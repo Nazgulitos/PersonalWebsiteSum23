5274742cdca370ae34050336dc17ca79f9fa7ff6
Назгуль Салихова <kokosiknn@MacBook-Air-Nazgul.local>
2023-07-12 10:38:48 +0300

    Update files with new changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,67 +1,67 @@
 <svelte:head>
-    <title>Jokes_API</title>
+	<title>Jokes_API</title>
 </svelte:head>
 
 <nav>
-    <a href="/#about" class="head">About</a>
-    <a href="/#projects" class="head">Projects</a>
-    <a href="/#contact" class="head">Contact</a>
-    <a href="/comic" class="head">Jokes_API</a>
+	<a href="/#about" class="head">About</a>
+	<a href="/#projects" class="head">Projects</a>
+	<a href="/#contact" class="head">Contact</a>
+	<a href="/comic" class="head">Jokes_API</a>
 </nav>
 
 <div class="wrapper">
-    <main>
-        <slot/>
-    </main>
+	<main>
+		<slot />
+	</main>
 
-    <footer>
-        <p>2023 Created by Salikhova Nazgul</p>
-    </footer>
+	<footer>
+		<p>2023 Created by Salikhova Nazgul</p>
+	</footer>
 </div>
 
 <style>
-    * {
-        margin: 0px;
-        padding: 0px;
-    }
+	* {
+		margin: 0px;
+		padding: 0px;
+	}
 
-    nav {
-        position: relative;
-        list-style-type: none;
-        width: 100%;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #161617;
-    }
-    a {
-        float: left;
-    }
-    a.head {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-family: 'Courier New', Courier, monospace;
-        font-size: 1.5em;
-    }
+	nav {
+		position: relative;
+		list-style-type: none;
+		width: 100%;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+		background-color: #161617;
+	}
+	a {
+		float: left;
+	}
+	a.head {
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+		font-family: 'Courier New', Courier, monospace;
+		font-size: 1.5em;
+	}
 
-    a.head:hover {
-        background-color: #403851;
-        transform: scale(1.04) perspective(1px)
-    }
+	a.head:hover {
+		background-color: #403851;
+		transform: scale(1.04) perspective(1px);
+	}
 
-    main {
-        flex: 1;
-        padding-bottom: 100px;
-    }
+	main {
+		flex: 1;
+		padding-bottom: 100px;
+	}
 
-    footer {
-        height: 100px;
-        text-align: center;
-        font-family: 'Abril Fatface', serif;
-        color: #4D4C4C;
-        font-size: 2em;
-    }
+	footer {
+		height: 100px;
+		text-align: center;
+		font-family: 'Abril Fatface', serif;
+		color: #4d4c4c;
+		font-size: 2em;
+	}
 </style>
--- src/routes/Contacts.svelte
+++ src/routes/Contacts.svelte
@@ -1,9 +1,11 @@
 <div id="contact2">
-    <a href="https://github.com/Nazgulitos" class="icon-github"> </a>
-    <a href="https://instagram.com/salikhova_nazgul_?igshid=OGQ5ZDc2ODk2ZA==" class="icon-insta"> </a>
-    <a href="https://t.me/kokosinka123" class="icon-tg"> </a>            
+	<a href="https://github.com/Nazgulitos" class="icon-github">
+		<a href="https://instagram.com/salikhova_nazgul_?igshid=OGQ5ZDc2ODk2ZA==" class="icon-insta">
+			<a href="https://t.me/kokosinka123" class="icon-tg">Telegram</a>
+		</a>
+	</a>
 </div>
 
 <style>
-    @import './app.css';
+	@import './app.css';
 </style>
